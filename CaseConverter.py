b051f2fde98e48b1d0be27fe567adb92f7c1ea62
azthief <choiyg81@gmail.com>
2016-11-21 15:52:09 +0900

    Update CaseConverter.py

--- CaseConverter.py
+++ CaseConverter.py
@@ -1,21 +1,22 @@
 import re
-import sublime
-import sublime_plugin
+import sublime, sublime_plugin
 
+try:
+  import commands
+except ImportError:
+  pass
 
 class converter1(sublime_plugin.TextCommand):
 	def run(self, edit):
-		# sublime.views()
-		data = 'SHMPT_NO_EXT'
-		dataLower = data.lower()
+		for region in self.view.sel():
+			if not region.empty():
+				data = self.view.substr(region)
+				print(data)
+				dataLower = data.lower()
 
-		regex = re.compile('(_[a-z])')
+				regex = re.compile('(_[a-z0-9])')
+				result = re.sub('(_[a-z0-9])', self.replaceUpper, dataLower)
+				self.view.replace(edit, region, result)
 
-		print(regex.findall(dataLower))
-		result = re.sub('(_[a-z])', replaceUpper, dataLower)
-		self.view.insert(edit, 0, result)
-
-	def replaceUpper(match):
+	def replaceUpper(self, match):
 		return match.group(0).upper().replace('_', '')
-
-
